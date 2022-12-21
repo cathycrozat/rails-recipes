09b292ce8c3f4db199ebd32e07d1c623eb1476af
cathy <catherinecrozat@gmail.com>
2022-12-21 11:06:56 +0100

    recipes first setups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "nokogiri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Recipe.destroy_all
+User.destroy_all
+
+puts "Creating user..."
+user1 = User.create!(username: "Cathy", email: "catherinecrozat@gmail.com", password: "toto123")
+photo = URI.open("https://ca.slack-edge.com/T02NE0241-U045TDQ4M43-d011388c1f01-512")
+user1.photo.attach(io: photo, filename: "profil")
+
+puts "Creating recipes..."
+recipe1 = Recipe.create!(dishname: "Tarte tatin", description: "Tarte tatin aux pommes déclicieuse", user: user1)
+photo = URI.open("https://assets.afcdn.com/recipe/20180503/79001_w1000h667c1cx2880cy1920.webp")
+recipe1.photo.attach(io: photo, filename: "profil")
